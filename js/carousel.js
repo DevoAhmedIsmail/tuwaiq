c502e43cb467ae4342452449d95b4b49ce561e31
Ahmed <deve.ahmedismail@gmail.com>
2023-02-26 16:07:00 +0200

    blog & photos & videos

--- js/carousel.js
+++ js/carousel.js
@@ -56,7 +56,7 @@ $(document).ready(function(){
               items:2
           },
           576:{
-              items:6
+              items:4
           },
           768:{
               items:8
